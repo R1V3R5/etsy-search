49e7c3b7a6bd078810aa0b1cadb64b66b2a8d7b0
Joshua Rivers <joshua.rivers@gmail.com>
2016-06-23 16:07:23 -0400

    Successfully building array of objects from etsy results and grabbing information needed

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,30 @@
-import $ from 'jquery'
+import $ from 'jquery';
 import {
   items
 }
-from './etsy'
+from './etsy';
+
+// console.log(items)
+
+var etsyItems = items.results.map(function(item) {
+  return {
+    shop_name: item.Shop.shop_name,
+    shop_url: item.Shop.url,
+    title: item.title,
+    url: item.url,
+    image: item.Images[0].url_170x135,
+    price: item.price
+  };
+})
+console.log(etsyItems)
+/*
+
+function imageTemplate (image) {
+  return `<div class="carousel">
+    <h3 class="image-title">${image.title}</h3>
+    <img src="${image.image_url}">
+    <p>Picture by: ${image.photographer_id}</p>
+  </div>`;
+}
+
+*/
