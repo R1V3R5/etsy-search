556f4889b4c8cbbe831ff8a1f39d75564323ae8c
Joshua Rivers <joshua.rivers@gmail.com>
2016-06-24 09:10:20 -0400

    have images, title, shop name, urls and price displaying in grid, just not pixel perfect

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,8 @@ var etsyItems = items.results.map(function(item) {
   };
 })
 console.log(etsyItems)
+
+
 /*
 
 function imageTemplate (image) {
@@ -26,5 +28,34 @@ function imageTemplate (image) {
     <p>Picture by: ${image.photographer_id}</p>
   </div>`;
 }
+images.forEach(function (x) {
+  var node = $(".board");
+  var imageHTML = imageTemplate(x);
+  node.append(imageHTML);
+});
 
+
+
+<div class="price">
+	<p>$${item.price}</p>
+</div>
 */
+
+function etsyTemplate(item) {
+  return `<div class="item-grid">
+     <div class="pic">
+        <a href="${item.url}"><img src="${item.image}"></a>
+     </div>
+     <div class="info">
+     	<p><a href="${item.url}">${item.title}</a></p>
+        <p><a href="${item.shop_url}">${item.shop_name}</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp$${item.price}</p>
+     </div
+   </div>`;
+}
+
+
+etsyItems.forEach(function(x) {
+  var node = $('.flex-column');
+  var etsyHTML = etsyTemplate(x);
+  node.append(etsyHTML);
+});
